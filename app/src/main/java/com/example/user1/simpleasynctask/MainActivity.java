20433194ee5d34bc112acac3b3e3279eb9606391
wilsontcs1201 <tancs-wa16@student.tarc.edu.my>
2018-12-10 12:54:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user1/simpleasynctask/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.user1.simpleasynctask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TEXT_STATE = "currentText";
+
+
+    private TextView mTextView = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //  Initialize mTextView
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+        // Restore TextView if there is a savedInstanceState
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+
+    public void startTask (View view) {
+        // Put a message in the text view
+        mTextView.setText("Napping.....");
+
+
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
